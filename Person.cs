25e8a73c1fdaaf7244f3a8fde094b0317a73e032
WhatIsFun <mohammed110051@gmail.com>
2023-07-12 11:37:14 +0400

    List example

---
+++ Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharp_Lists
+{
+    internal class Person : IComparable<Person>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+
+        public Person(int Id, string Name, string Gender, int Age) 
+        { 
+            this.Id = Id;
+            this.Name = Name;
+            this.Gender = Gender;
+            this.Age = Age;
+        }
+        public int CompareTo(Person? other)
+        {
+            if (other == null) return 1;
+            if (other.Age == this.Age) return 0;
+            if (other.Age > this.Age) return -1;
+            return 1;
+        }
+        public void ToString()
+        {
+            Console.WriteLine($"\r\nName: {Name}");
+            Console.WriteLine($"Id: {Id}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"Gender: {Gender}");
+            Console.WriteLine($"\r\n~~~~~~~~~~~~~~~~~~");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace cSharp_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // List:
+            List<int> list = new List<int>();
+            list.Add (1);
+            Console.WriteLine($"Size of the list is: {list.Count}");
+            list.Add (2);
+            Console.WriteLine($"Size of the list is: {list.Count}");
+            list.Remove(1);
+            Console.WriteLine($"Size of the list is: {list.Count}");
+            list.Insert (0, 5);
+            Console.WriteLine($"Size of the list is: {list.Count}");
+
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+
+            List <Person> personList = new List<Person>();
+            personList.Add(new Person(12399, "Mohd", "Male", 32));
+            personList.Add(new Person(14569, "fda", "Female",12));
+            personList.Add(new Person(23559, "ffdn", "Male",19));
+
+            
+
+            personList.Sort();
+            Console.WriteLine("Sorting by age:");
+            
+            foreach (Person person in personList)
+            {
+                Console.WriteLine($"\r\nName: {person.Name}");
+                Console.WriteLine($"Id: {person.Id}");
+                Console.WriteLine($"Age: {person.Age}");
+                Console.WriteLine($"Gender: {person.Gender}");
+                Console.WriteLine($"\r\n~~~~~~~~~~~~~~~~~~");
+            }
+
+
+
+            List<int> numberList = new List<int>();
+            numberList.Add(2);
+            numberList.Add(4);
+            numberList.Add(6);
+            numberList.Add(15);
+            numberList.Add(9);
+
+            numberList.Sort();
+            Console.WriteLine("\r\nAfter Sorting...");
+            foreach (int i in numberList)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
